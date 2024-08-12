99ff448d0d4ce88a2d404c9e49d33fd13ee92b1b
Manisha Goyal <manisha.goyal1996@gmail.com>
2024-08-12 18:59:27 -0400

    removed live deployment from projects tab

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -102,19 +102,6 @@ const Projects = () => {
               <div className="border border-white/20"></div>
               {/* buttons */}
               <div className="flex items-center gap-4">
-                {/* live project button */}
-                <Link href={project.live}>
-                  <TooltipProvider delayDuration={100}>
-                    <Tooltip>
-                      <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
-                        <BsArrowUpRight className="text-white text-3xl group-hover:text-accent" />
-                      </TooltipTrigger>
-                      <TooltipContent>
-                        <p>Live project</p>
-                      </TooltipContent>
-                    </Tooltip>
-                  </TooltipProvider>
-                </Link>
                 {/* github button */}
                 <Link href={project.github}>
                   <TooltipProvider delayDuration={100}>
