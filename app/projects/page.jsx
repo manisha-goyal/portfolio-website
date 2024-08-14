c9995368281da314488d6dd8b4579a40a565ba8d
Manisha Goyal <manisha.goyal1996@gmail.com>
2024-08-14 00:06:54 -0400

    Updated projects

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,10 +1,7 @@
 "use client";
 
-import React, { useState } from "react";
-import { BsArrowUpRight, BsGithub } from "react-icons/bs";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/swiper-bundle.css";
-
+import React from "react";
+import { BsGithub } from "react-icons/bs";
 import Link from "next/link";
 import Image from "next/image";
 import { motion } from "framer-motion";
@@ -17,54 +14,34 @@ import {
   TooltipTrigger,
 } from "@/components/ui/tooltip";
 
-import WorkSliderBtns from "@/components/WorkSliderBtns";
-
 const projects = [
   {
-    num: "01",
-    category: "frontend",
-    title: "project 1",
+    title: "Bookstore Database & Recommendation System",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, consectetur.",
-    stack: [{ name: "Html 5" }, { name: "CSS 3" }, { name: "Javascript" }],
+      "An advanced bookstore database system integrating Machine Learning algorithms for inventory management and personalized recommendations",
+    technologies: "Html 5, CSS 3, Javascript",
     image: "/assets/projects/thumb1.png",
-    live: "",
     github: "",
   },
   {
-    num: "02",
-    category: "fullstack",
-    title: "project 2",
+    title: "Shell Implementation",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, consectetur.",
-    stack: [{ name: "Next.js" }, { name: "Tailwind CSS" }, { name: "Node.js" }],
+      "A simplified UNIX shell in C, incorporating features like basic command execution, input/output redirection, pipe-based inter-process communication, and job control for process management",
+    technologies: "Html 5, Tailwind CSS, Javascript",
     image: "/assets/projects/thumb2.png",
-    live: "",
     github: "",
   },
   {
-    num: "03",
-    category: "frontend",
-    title: "project 3",
+    title: "Analysis of Library Training Effectiveness",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, consectetur.",
-    stack: [{ name: "Next.js" }, { name: "Tailwind CSS" }],
+      "Research on effectiveness of SMU Library's training efforts on students' research skills through statistical and text analysis on survey data from 1000 students",
+    technologies: "Html 5, CSS 3, Javascript",
     image: "/assets/projects/thumb3.png",
-    live: "",
     github: "",
   },
 ];
 
 const Projects = () => {
-  const [project, setProject] = useState(projects[0]);
-
-  const handleSlideChange = (swiper) => {
-    // get current slide index
-    const currentIndex = swiper.activeIndex;
-    // update project state based on current slide index
-    setProject(projects[currentIndex]);
-  };
-
   return (
     <motion.div
       initial={{ y: "-200vh" }}
@@ -73,84 +50,49 @@ const Projects = () => {
       className="min-h-[80vh] flex items-center justify-center py-12 xl:py-0"
     >
       <div className="container mx-auto">
-        <div className="flex flex-col xl:flex-row xl:gap-[30px]">
-          <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
-            <div className="flex flex-col gap-[30px] h-[50%]">
-              {/* outline num */}
-              <div className="text-8xl leading-none font-extrabold text-transparent text-outline">
-                {project.num}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+          {projects.map((project, index) => (
+            <div
+              key={index}
+              className="flex flex-col gap-4 bg-secondary p-4 rounded-lg"
+            >
+              <div className="relative w-full h-[200px] bg-pink-50/20">
+                {/* overlay */}
+                <div className="absolute top-0 bottom-0 w-full h-full bg-black/10 z-10"></div>
+                {/* image */}
+                <div className="relative w-full h-full">
+                  <Image
+                    src={project.image}
+                    alt={project.title}
+                    fill
+                    className="object-cover"
+                  />
+                </div>
               </div>
-              {/* project category */}
-              <h2 className="text-[42px] font-bold leading-none transition-all duration-500 capitalize">
-                {project.category} project
-              </h2>
-              {/* project description */}
-              <p className="text-white/60">{project.description}</p>
-              {/* stack */}
-              <ul className="flex gap-4">
-                {project.stack.map((item, index) => {
-                  return (
-                    <li key={index} className="text-xl text-accent">
-                      {item.name}
-                      {/* remove the last comma */}
-                      {index !== project.stack.length - 1 && ","}
-                    </li>
-                  );
-                })}
-              </ul>
-              {/* border */}
-              <div className="border border-white/20"></div>
-              {/* buttons */}
-              <div className="flex items-center gap-4">
-                {/* github button */}
-                <Link href={project.github}>
-                  <TooltipProvider delayDuration={100}>
-                    <Tooltip>
-                      <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
-                        <BsGithub className="text-white text-3xl group-hover:text-accent" />
-                      </TooltipTrigger>
-                      <TooltipContent>
-                        <p>Github repository</p>
-                      </TooltipContent>
-                    </Tooltip>
-                  </TooltipProvider>
-                </Link>
+              <div className="flex flex-col gap-4 flex-1">
+                <h2 className="text-2xl font-bold text-white">
+                  {project.title}
+                </h2>
+                <p className="text-white/80 flex-1">{project.description}</p>
+                <div className="border border-white/20"></div>
+                <p className="text-white/70">{project.technologies}</p>
+                <div className="flex justify-end mt-4">
+                  <Link href={project.github}>
+                    <TooltipProvider delayDuration={100}>
+                      <Tooltip>
+                        <TooltipTrigger className="w-[30px] h-[30px] rounded-full bg-white/5 flex justify-center items-center group">
+                          <BsGithub className="text-white text-3xl group-hover:text-accent" />
+                        </TooltipTrigger>
+                        <TooltipContent>
+                          <p>Github repository</p>
+                        </TooltipContent>
+                      </Tooltip>
+                    </TooltipProvider>
+                  </Link>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="w-full xl:w-[50%]">
-            <Swiper
-              spaceBetween={30}
-              slidesPerView={1}
-              className="xl:h-[520px] mb-12"
-              onSlideChange={handleSlideChange}
-            >
-              {projects.map((project, index) => {
-                return (
-                  <SwiperSlide key={index} className="w-full">
-                    <div className="h-[460px] relative group flex justify-center items-center bg-pink-50/20">
-                      {/* overlay */}
-                      <div className="absolute top-0 bottom-0 w-full h-full bg-black/10 z-10"></div>
-                      {/* image */}
-                      <div className="relative w-full h-full">
-                        <Image
-                          src={project.image}
-                          alt={project.title}
-                          fill
-                          className="object-cover"
-                        />
-                      </div>
-                    </div>
-                  </SwiperSlide>
-                );
-              })}
-              {/* slider buttons */}
-              <WorkSliderBtns
-                containerStyles="flex gap-2 absolute right-0 bottom-[calc(50%_-_22px)] xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none"
-                btnStyles="bg-accent hover:bg-accent-hover text-primary text-[22px] w-[44px] h-[44px] flex justify-center items-center transition-all"
-              />
-            </Swiper>
-          </div>
+          ))}
         </div>
       </div>
     </motion.div>
